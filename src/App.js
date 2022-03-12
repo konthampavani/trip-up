825e73d713ef1d45f44f5cdb44b7064c458aa41b
konthampavani <76587244+konthampavani@users.noreply.github.com>
2022-03-13 04:00:37 +0530

    She_hack_group14

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Home from './Home';
+import { BrowserRouter , Route ,Routes } from "react-router-dom";
+import Services from './Services';
+import Pd from './Pd';
+import Search from './Search';
+import Weather from "./Components/Weather";
+import Hotel from "./Components/Hotel";
+import Ng from "./Components/Ng";
+import Flight from "./Components/Flight";
+import Error404 from './Components/Error404';
+import Login from './Components/Login';
+import Delhi from './Pages/Delhi';
+import Mumbai from './Pages/Mumbai';
+import Goa from './Pages/Goa';
+import Jaipur from './Pages/Jaipur';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+      <Route path="/Login" exact  element={<Login/>} />
+        <Route path="/" exact  element={<Home/>} />
+        <Route path="/services" exact  element={<Services/>} />
+        <Route path="/Pd" exact  element={<Pd/>}/>
+        <Route path="/Search" exact  element={<Search/>} >
+         <Route path="Weather" exact  element={<Weather/>} />
+         <Route path="Hotel" exact  element={<Hotel/>} />
+         <Route path="Ng" exact  element={<Ng/>} />
+         <Route path="Flight" exact  element={<Flight/>} />
+        </Route>
+        <Route path="Delhi" exact  element={<Delhi/>} />
+        <Route path="Mumbai" exact  element={<Mumbai/>} />
+        <Route path="Goa" exact  element={<Goa/>} />
+        <Route path="Jaipur" exact  element={<Jaipur/>} />
+        <Route path="*" element={<Error404/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
